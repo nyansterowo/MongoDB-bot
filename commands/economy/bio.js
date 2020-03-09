b251397e9bdf65fc47313ca1517f90babdc7d0c0
ThePersigg <49908303+perssBest@users.noreply.github.com>
2020-03-09 14:03:37 +0500

    Create bio.js

---
+++ commands/economy/bio.js
@@ -0,0 +1,20 @@
+module.exports = {
+  name: 'bio',
+  description: 'Изменить описание.',
+  aliases: [],
+  public: true,
+  async execute(bot, message, args) {
+    User.findOne({guildID: message.guild.id, userID: message.author.id},(err,data) => {
+    let context = args.slice(0).join(` `)
+    if(!context){ data.bio = `\`\`\`<prefix>bio [Текст]\`\`\``; data.save(); message.react("🎉"); return; }
+    if(context.length >= 200) return message.reply(`К сожелению я не могу поставить вам такое описание. Оно имеет ${context.length}длинну.`)
+    let a = new Discord.MessageEmbed()
+    .setTitle(`Вы успешно изменили свою биографию.`)
+    .setDescription(context)
+    .setColor(config.color)
+    message.channel.send(a)
+      data.bio = context;
+      data.save()
+    })
+  }
+}
